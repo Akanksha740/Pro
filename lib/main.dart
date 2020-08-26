bee0b197ae8626e8f2738f44e8407afb50ed19eb
AKANKSHA <akanksha.1999.sharma@gmail.com>
2020-08-26 12:21:36 +0530

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,115 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('NOGOZO'),
+        actions: [
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: [
+            //header
+            new UserAccountsDrawerHeader(
+              accountName: Text('Akanksha'),
+              accountEmail: Text('akanksha03@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                  child: new CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.person,
+                  color: Colors.grey,
+                ),
+              )),
+              decoration: new BoxDecoration(color: Colors.blue),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Wishlist'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Contact us'),
+                leading: Icon(Icons.phone),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
